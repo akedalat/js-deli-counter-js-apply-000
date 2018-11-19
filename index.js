fcb62e666ab234d358c8e0be2cb5bf9dc5f19ea9
Kian Edalat <akedalat@gmail.com>
2018-11-19 13:18:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function takeANumber (line, name){
+katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${line.lenght} in line.`)
+
+};
 function takeANumber(line, name) {
   line.push(name)
 
