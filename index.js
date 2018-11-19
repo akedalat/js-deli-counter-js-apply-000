8b8ccc6e00da82eca838f801e0b622c6c8d3cf24
Kian Edalat <akedalat@gmail.com>
2018-11-19 13:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function nowServing(line) {
   if (!line.length) {
     return "There is nobody waiting to be served!"
   }
-
-  return `Currently serving ${line.shift()}.`
+    return `currently serving ${line.shift()}.`
 };
+
