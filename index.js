cc5813eff536631eab67269e8a45e2a6750a3ec5
Kian Edalat <akedalat@gmail.com>
2018-11-19 14:33:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function currentLine (line){
   if (!line.length) {
     return "The line is currently empty."
   }
-var string = "The line is currently"
+var string = "The line is currently:"
 for (var i = 0; i < line.length; i++) {
 
-string = string + `: ${i+1}. ${line[i]}.`
+string = string + ` ${i+1}. ${line[i]}.`
 
 
 }
