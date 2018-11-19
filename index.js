1d54015ad1a74bec96ffbbc588d27f58486d6e4f
Kian Edalat <akedalat@gmail.com>
2018-11-19 13:34:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ function takeANumber(line, name) {
 };
 
 function nowServing (line){
-let i = 0;
-while (i<line.lenght) {
+
   if (line.lenght = 0) {
     return 'There is nobody waiting to be served!'
   } else {
     return line [i];
     line.shift();
-  }
-i++;
 }
 }
