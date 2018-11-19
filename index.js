c1ba18f597818b3b860624ff84a35bfe400948a2
Kian Edalat <akedalat@gmail.com>
2018-11-19 13:26:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ katzDeliLine.push(name);
   return (`Welcome, ${name}. You are number ${line.lenght} in line.`)
 };
 
-function nowServing (){
-  
+function nowServing (katzDeliLine){
+let i = 0;
+while (i<katzDeliLine.lenght) {
+  if (katzDeliLine.lenght = 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return katzDeliLine [i];
+  }
+i++;
+}
 };
